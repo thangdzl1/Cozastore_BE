089f2f99536b3f29f1027b1486aa97b013bcb77a
BQcThang <quocthang6a1@gmail.com>
2023-07-11 19:18:30 +0700

    api upload, download file and custom exception

---
+++ src/main/java/com/cybersoft/cozastore/controller/DemoUploadFileController.java
@@ -0,0 +1,75 @@
+package com.cybersoft.cozastore.controller;
+
+import com.cybersoft.cozastore.exception.CustomFileNotFoundException;
+import com.cybersoft.cozastore.payload.request.SignupRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.xml.crypto.dsig.spec.XPathFilterParameterSpec;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLOutput;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoUploadFileController {
+
+//    Chứa toàn bộ hàm hỗ trợ sẵn liên quan tới đường dẫn
+
+    @Value("${path.root}")
+    private String spath;
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> loadFile(@PathVariable String fileName) {
+//      Đường dẫn folder root lưu hình
+        try {
+            Path rootPath = Paths.get(spath);
+            Resource resource = new UrlResource(rootPath.resolve(fileName).toUri());
+            if (resource.exists()) {
+//      Nếu tồn tại thì mới cho phép download
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                        .body(resource);
+            } else {
+//      Khi ném exception thì code sẽ dừng và văng ra lỗi
+                throw new CustomFileNotFoundException(200, "file not found");
+            }
+        } catch (Exception e) {
+            throw new CustomFileNotFoundException(200, "file not found");
+        }
+    }
+
+    @PostMapping("/uploadfile")
+    public ResponseEntity<?> uploadFile(
+            @RequestParam MultipartFile file
+    ) {
+//      Định nghĩa đường dẫn
+        Path rootPath = Paths.get(spath);
+
+        try {
+            if (!Files.exists(rootPath)) {
+//      Tạo folder ứng với lại đường dẫn nếu không tồn tại folder
+                Files.createDirectories(rootPath);
+            }
+
+//      Resolve tương đương với dấu /
+//      file.getOriginalFilename() : lấy tên file và định dạng
+            String fileName = file.getOriginalFilename();
+            Files.copy(file.getInputStream(), rootPath.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            System.out.println("Loi" + e.getLocalizedMessage());
+        }
+
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+
+}
+
