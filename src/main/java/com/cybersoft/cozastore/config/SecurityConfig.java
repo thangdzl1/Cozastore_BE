3c30a68feeb84548642448662846e656b8428d62
BQcThang <quocthang6a1@gmail.com>
2024-03-20 14:56:51 +0700

    update SecurityConfig/corsConfigurationSource

--- src/main/java/com/cybersoft/cozastore/config/SecurityConfig.java
+++ src/main/java/com/cybersoft/cozastore/config/SecurityConfig.java
@@ -15,7 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 
 
 @Configuration
@@ -42,11 +46,24 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        // cấu hình lại cors cho các req sử dụng headers Authorization
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); //cho phép tất cả các domain gọi đến
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));//cho phép các phương thức gọi đến
+        configuration.setAllowedHeaders(Arrays.asList("*"));//cho phép tất cả các header gọi đến
+        configuration.setAllowCredentials(true);//cho phép gửi cookie
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);//qui định cấu hình cors cho tất cả các link
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()//tắt hình thức cấu hình liên quan đến tấn công CSRF
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//khai báo không sử dụng session
                 .and()
-                .cors()
+                .cors().configurationSource(corsConfigurationSource())
                 .and()
                 .authorizeHttpRequests()//qui định lại các rule liên quan đến chứng thực cho link được gọi
                     .antMatchers("/signin"
