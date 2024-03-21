b54c9d36b1e4297178b6f629ba4b52918e47f293
BQcThang <quocthang6a1@gmail.com>
2024-03-21 23:42:12 +0700

    add API SizeController

--- src/main/java/com/cybersoft/cozastore/controller/SizeController.java
+++ src/main/java/com/cybersoft/cozastore/controller/SizeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +17,10 @@ public class SizeController {
     private SizeServiceImp sizeServiceImp;
 
     @GetMapping("")
-    public ResponseEntity<?> findAllSizeByProductName(String name){
+    public ResponseEntity<?> findAllSizeByProductName(@RequestParam String productName){
 
         BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setData(sizeServiceImp.findAllSizeByProductName(name));
+        baseResponse.setData(sizeServiceImp.findAllSizeByProductName(productName));
         baseResponse.setStatusCode(200);
 
         return new ResponseEntity<>(baseResponse, HttpStatus.OK);
