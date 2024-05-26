f09adfaa132efe4abe7a97f3053f7bcc40a58fd3
BQcThang <quocthang6a1@gmail.com>
2024-05-27 01:58:51 +0700

    rearange category service

--- src/main/java/com/cybersoft/cozastore/service/CategoryService.java
+++ src/main/java/com/cybersoft/cozastore/service/CategoryService.java
@@ -29,11 +29,11 @@ public class CategoryService implements CategoryServiceImp {
     @Override
 //    @Cacheable("listCategory")
     public List<CategoryResponse> getAllCategory() {
-        System.out.println("Kiem tra category");
+//        System.out.println("Kiem tra category");
         List<CategoryResponse> categoryResponseList = new ArrayList<>();
 
         if(!redisTemplate.hasKey("listCategory")){
-            System.out.println("khong co gia tri tren redis");
+//            System.out.println("khong co gia tri tren redis");
             List<CategoryEntity> list = categoryRepository.findAll();
             for (CategoryEntity data : list) {
                 CategoryResponse categoryResponse = new CategoryResponse();
@@ -47,7 +47,7 @@ public class CategoryService implements CategoryServiceImp {
 
             redisTemplate.opsForValue().set("listCategory",data);
         }else{
-            System.out.println("co gia tri tren Redis");
+//            System.out.println("co gia tri tren Redis");
             String data = redisTemplate.opsForValue().get("listCategory").toString();
             Type listType = new TypeToken<ArrayList<CategoryResponse>>(){}.getType();
             categoryResponseList = new Gson().fromJson(data, listType);
